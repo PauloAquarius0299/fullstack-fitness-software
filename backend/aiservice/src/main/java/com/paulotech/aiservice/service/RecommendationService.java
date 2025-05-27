902774d0b114ce0f6835a6b3778a2289e39dccc2
Paulo Cesar <paulocgsantos99@gmail.com>
2025-05-27 17:50:03 -0300

    feat: add recommendations functionalities

---
+++ backend/aiservice/src/main/java/com/paulotech/aiservice/service/RecommendationService.java
@@ -0,0 +1,23 @@
+package com.paulotech.aiservice.service;
+
+import com.paulotech.aiservice.model.Recommendation;
+import com.paulotech.aiservice.repository.RecommendationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationService {
+    private final RecommendationRepository recommendationRepository;
+
+    public List<Recommendation> getUserRecommendation(String userId) {
+        return recommendationRepository.findByUserId(userId);
+    }
+
+    public Recommendation getActivityRecommendation(String activityId) {
+        return recommendationRepository.findByActivityId(activityId)
+                .orElseThrow(() -> new RuntimeException("No recommendations found for activity ID: " + activityId));
+    }
+}
