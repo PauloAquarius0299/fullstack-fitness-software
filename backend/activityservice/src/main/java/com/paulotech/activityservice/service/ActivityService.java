306372603ac7de97061d87d2a14b533c95f99a77
Paulo Cesar <paulocgsantos99@gmail.com>
2025-05-28 17:56:31 -0300

    feat: add rabbitmq template service and comments validUserId

--- backend/activityservice/src/main/java/com/paulotech/activityservice/service/ActivityService.java
+++ backend/activityservice/src/main/java/com/paulotech/activityservice/service/ActivityService.java
@@ -3,8 +3,12 @@ package com.paulotech.activityservice.service;
 import com.paulotech.activityservice.dto.ActivityRequest;
 import com.paulotech.activityservice.dto.ActivityResponse;
 import com.paulotech.activityservice.entity.Activity;
+import com.paulotech.activityservice.exception.UserNotFoundException;
 import com.paulotech.activityservice.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,17 +16,24 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ActivityService {
 
     private final ActivityRepository repository;
     private final UserValidationService userValidationService;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
 
     public ActivityResponse trackActivity(ActivityRequest activityRequest) {
-        boolean isValidUser = userValidationService.validateUser(activityRequest.getUserId());
-        if(!isValidUser) {
-            throw new RuntimeException("Invalid User" + activityRequest.getUserId());
-        }
-        Activity activity = new Activity().builder()
+//        boolean isValidUser = userValidationService.validateUser(activityRequest.getUserId());
+//        if(!isValidUser) {
+//            throw new UserNotFoundException("Invalid User" + activityRequest.getUserId());
+//        }
+        Activity activity = Activity.builder()
                 .userId(activityRequest.getUserId())
                 .type(activityRequest.getType())
                 .duration(activityRequest.getDuration())
@@ -31,6 +42,11 @@ public class ActivityService {
                 .additionalMetrics(activityRequest.getAdditionalMetrics())
                 .build();
         Activity savedActivity = repository.save(activity);
+        try {
+            rabbitTemplate.convertAndSend(exchange, routingKey, savedActivity);
+        } catch(Exception e){
+            log.error("Failed to send activity to RabbitMQ", e);
+        }
         return mapToResponse(activity);
     }
 
